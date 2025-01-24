4edc10cf4514a77a31f76d193bf45fad6d7825b6
majunheng <ma13051650075@163.com>
2025-01-24 16:24:18 +0800

    众神接口脚本version：2

--- testcases_众神_接口测试/test_13_send_gift_发道具.py
+++ testcases_众神_接口测试/test_13_send_gift_发道具.py
@@ -12,11 +12,11 @@ product_id = config['config']['product_id']
 server_id = config['config']['server_id']
 
 
-@pytest.mark.parametrize("star,end,step", [(100022, 100022, 1)])
+@pytest.mark.parametrize("star,end,step", [(99001, 99006, 1)])
 def test_send_gift(star, end, step):
     for i in range(star, end + 1, step):
-        # api_url = f"{url_90}/debug/addProduct?open_id={openid}&product_id={i}&num={num}&server_id={server_id}"
-        api_url = f"{url_IDC}/debug/addProduct?open_id={openid}&product_id={i}&num={num}&server_id={server_id}"
+        api_url = f"{url_90}/debug/addProduct?open_id={openid}&product_id={i}&num={num}&server_id={server_id}"
+        # api_url = f"{url_IDC}/debug/addProduct?open_id={openid}&product_id={i}&num={num}&server_id={server_id}"
         # 执行相关操作
         response = requests.post(api_url)
         assert response.status_code == 200
